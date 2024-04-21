3d171a130eb4ef8c236ab36bb19b0f8e089832c8
genaro gonzalez <genarrogg@gmail.com>
2024-04-21 15:51:34 -0400

    animcion a las tarjetas

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["351993d657c064e61ed291d75484d8b4.ipfscdn.io"],
+  },
 };
 
 export default nextConfig;
--- src/components/views/shop/NFTCard.tsx
+++ src/components/views/shop/NFTCard.tsx
@@ -8,6 +8,8 @@ import { NFT } from "@thirdweb-dev/sdk";
 import { TOOLS_ADDRESS } from "@constant/contratos";
 import { ethers } from "ethers";
 
+import Image from "next/image";
+
 type Props = {
   nft: NFT;
 };
@@ -20,33 +22,40 @@ export default function NFTComponent({ nft }: Props) {
   );
 
   return (
-    <div style={{ overflow: "hidden" }}>
-      <MediaRenderer src={nft.metadata.image} height="100%" width="100%" />
-      <h2
-        style={{
-          fontSize: "2em",
-          fontWeight: "bold",
-          margin: "1em 0",
-          textAlign: "center",
-        }}
-      >
-        {nft.metadata.name}
-      </h2>
+    <div className="nft">
+      {/* <MediaRenderer src={nft.metadata.image} height="100%" width="100%" /> */}
+
+      <div className="img">
+        {nft.metadata.image && (
+          <Image
+            src={nft.metadata.image}
+            alt="Descripción de la imagen"
+            objectFit="cover"
+            height={400}
+            width={360}
+          />
+        )}
+      </div>
+      <div className="nombre">
+        <h2>{nft.metadata.name}</h2>
+      </div>
+
       {!isLoading && data ? (
-        <p style={{ textAlign: "center", margin: "1em 0" }}>
+        <p className="costo">
           Cost: {ethers.utils.formatEther(data?.price)}
           {" " + data?.currencyMetadata.symbol}
         </p>
       ) : (
-        <p>Loading...</p>
+        <p className="costo">Loading...</p>
       )}
-      <Web3Button
-        contractAddress={TOOLS_ADDRESS}
-        action={(contract) => contract.erc1155.claim(nft.metadata.id, 1)}
-        style={{ display: "block", margin: "0 auto" }}
-      >
-        Buy
-      </Web3Button>
+      <div className="btn">
+        <Web3Button
+          contractAddress={TOOLS_ADDRESS}
+          action={(contract) => contract.erc1155.claim(nft.metadata.id, 1)}
+        >
+          Buy NFT
+        </Web3Button>
+      </div>
     </div>
   );
 }
