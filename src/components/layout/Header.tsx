49beb555ab3c0e6af528f3125f2d5d3e5b81e21c
genaro gonzalez <genarrogg@gmail.com>
2024-05-22 11:03:07 -0400

    colores del header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import Link from "next/link";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
- /*  const address = useAddress(); */
-
+  /*  const address = useAddress(); */
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
       <div className="desktop">
         <h2>Defender Warriors</h2>
-        <nav>
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className={`hamburguesa ${isOpen ? "active" : ""}`}>
           <ul>
             <li>
               <a href="/">Play</a>
